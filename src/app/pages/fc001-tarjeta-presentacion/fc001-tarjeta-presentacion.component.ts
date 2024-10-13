5ad4c7ba015811024b5fe9d6f2ab1ef0752875c0
vantabytes <romero.nico18@gmail.com>
2024-10-13 20:29:53 -0300

    feat: add fc001 tarjeta presentacion

---
+++ src/app/pages/fc001-tarjeta-presentacion/fc001-tarjeta-presentacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fc001-tarjeta-presentacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './fc001-tarjeta-presentacion.component.html',
+  styleUrl: './fc001-tarjeta-presentacion.component.scss'
+})
+export class FC001TarjetaPresentacionComponent {
+
+}
