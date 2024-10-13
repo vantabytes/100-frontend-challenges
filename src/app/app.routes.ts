158ce14ef410526b39bb4c67f9b8110b6c9dec30
vantabytes <romero.nico18@gmail.com>
2024-10-13 19:15:47 -0300

    feat: move layout to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FC001TarjetaPresentacionComponent } from './pages/fc001-tarjeta-presentacion/fc001-tarjeta-presentacion.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  {
+    path: 'fc001-tarjeta-presentacion',
+    component: FC001TarjetaPresentacionComponent,
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
