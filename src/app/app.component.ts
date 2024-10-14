b404dfeed555a004e98f87585c658ef89707dfad
vantabytes <romero.nico18@gmail.com>
2024-10-14 18:00:13 -0300

    feat: update project styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BackgroundComponent } from './components/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet],
+  template: `<router-outlet /> `,
 })
 export class AppComponent {}
