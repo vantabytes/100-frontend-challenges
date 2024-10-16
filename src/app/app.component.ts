ef1b1f269df4289d40bee463cb6c98524b95b8f7
vantabytes <romero.nico18@gmail.com>
2024-10-15 21:59:44 -0300

    feat: add navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet /> `,
+  imports: [RouterOutlet, NavigationComponent],
+  template: ` <app-navigation />
+    <router-outlet />`,
 })
 export class AppComponent {}
